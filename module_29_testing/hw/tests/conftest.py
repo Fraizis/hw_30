26389ed3b16f5300012d4f4a52797016db1e208d
fraizis <fraizis@mail.ru>
2025-06-08 20:23:50 +0300

    hw_30

---
+++ module_29_testing/hw/tests/conftest.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+import pytest
+
+from module_29_testing.hw.app.models import Client, Parking, ClientParking
+from module_29_testing.hw.main import create_app, db as _db
+
+
+@pytest.fixture
+def app():
+    _app = create_app()
+    _app.config["TESTING"] = True
+    _app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+
+    with _app.app_context():
+        _db.create_all()
+        client_1 = Client(
+            name="name",
+            surname="surname",
+            credit_card='credit_card',
+            car_number='car_number'
+        )
+        client_2 = Client(
+            name="name",
+            surname="surname",
+            credit_card='credit_card',
+            car_number='car_number'
+        )
+        client_3 = Client(
+            name="name",
+            surname="surname",
+            credit_card=None,
+            car_number='car_number'
+        )
+        parking_1 = Parking(
+            address="address",
+            opened=1,
+            count_places=10,
+            count_available_places=5
+        )
+        parking_2 = Parking(
+            address="address",
+            opened=0,
+            count_places=5,
+            count_available_places=5
+        )
+        parking_3 = Parking(
+            address="address",
+            opened=1,
+            count_places=5,
+            count_available_places=0
+        )
+        client_parking_1 = ClientParking(
+            client_id=1,
+            parking_id=1,
+            time_in=datetime.now(),
+            time_out=datetime.now()
+        )
+        client_parking_2 = ClientParking(
+            client_id=3,
+            parking_id=1,
+            time_in=datetime.now(),
+        )
+
+        _db.session.add_all([client_1, client_2, client_3])
+        _db.session.add_all([parking_1, parking_2, parking_3])
+        _db.session.add_all([client_parking_1, client_parking_2])
+        _db.session.commit()
+
+        yield _app
+        _db.session.close()
+        _db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    client = app.test_client()
+    yield client
+
+
+@pytest.fixture
+def db(app):
+    with app.app_context():
+        yield _db
---
+++ module_29_testing/hw/tests/test_with_factory.py
@@ -0,0 +1,16 @@
+from .factories import ClientFactory, ParkingFactory
+from ..app.models import Client, Parking
+
+
+def test_create_client(app, db):
+    client_create = ClientFactory()
+    db.session.commit()
+    assert client_create.id is not None
+    assert len(db.session.query(Client).all()) == 4
+
+
+def test_create_parking(client, db):
+    parking = ParkingFactory()
+    db.session.commit()
+    assert parking.id is not None
+    assert len(db.session.query(Parking).all()) == 4
